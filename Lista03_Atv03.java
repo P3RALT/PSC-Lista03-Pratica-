ab1ff0e5e649bbfdc831a2a52c44df5447f2d687
Gabriel Machado <127336276+P3RALT@users.noreply.github.com>
2025-06-18 11:09:28 -0300

    Add files via upload

---
+++ Lista03_Atv03.java
@@ -0,0 +1,14 @@
+package lista03.atv03;
+public class Lista03_Atv03 {
+  public static void main(String[] args){
+  int numero = 233;
+    while(numero <= 456){
+    System.out.println(numero); 
+    if (numero >= 300 && numero <= 400){ 
+    numero += 5; }
+     else {
+            numero += 3; }
+    } 
+}
+} 
+
